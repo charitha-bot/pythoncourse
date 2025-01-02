1d4c03c73c553c6b3de5a1acc94d291d5fe8fd86
Charitha Challa <charitha.challa522@gmail.com>
2025-01-02 21:17:33 +0530

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,27 @@
+# Comments, Escape sequences and print statements
+# This is the single line comment
+print("Hello World")
+# This is the multi-line comment, there are 2 types
+'''
+This is the single quoted multiline comment
+represented by single quote
+'''
+print("Hi Python")
+"""
+This is the double quoted multiline comment
+represented by double quote
+"""
+print("Hi Replit")
+# For commenting and uncommenting the lines, we can use ctrl+/.
+# Escape sequences
+# For new line insert \n at the end of the line
+print(
+    "The viewer is the great person \n and the messenger is the great person too"
+)
+# If need any "" in the line add \ before the ""
+print("Hey, this \"Viewer\" is a great preson")
+# If used single quote then
+print('This is python \'by replit\' platform')
+# Separate the values with sep="~", end the values by end=""
+print("Python", 3, 12, sep="~", end="02\n")
+print("Bye")
