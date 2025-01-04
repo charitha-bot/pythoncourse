733e37dcbc92a97a68891cfcafb0637f8446957e
Charitha Challa <charitha.challa522@gmail.com>
2025-01-04 22:38:52 +0530

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,19 @@
+# Variables and Datatypes
+a = 1
+print("The type of a is", type(a))
+b = 2.1
+print("The type of b is", type(b))
+# Complex is not written as 6+2i, it is written as complex(6,2)
+c = complex(6, 2)
+print("The type of c is", type(c))
+d = "Hello"
+print("The type of d is", type(d))
+e = True
+print("The type of e is", type(e))
+list = [1, 2, 3, 4, 2.2, "Hello"]
+print("The type of list is", type(list))
+tuple = {"Hello",1}
+print("The type of tuple is", type(tuple))
+#Dictionary == key value pairs
+dict = {"Name":"Sakshi", "Age":20, "CanVote":True}
+print("The type of dict is", type(dict))
