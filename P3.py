fe492e14f8ea22aa77a6391e8fd185b567317a5b
Charitha Challa <charitha.challa522@gmail.com>
2025-01-06 21:59:16 +0530

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,20 @@
+# Operators
+print(12 + 3)
+print(12 - 3)
+print(12 * 3)
+print(12 / 3)
+#The next operation is floor division operator
+print(12 // 3)
+#The next operation is modulus operator
+print(12 % 5)
+#The next operation is exponential operator
+print(5**3)  # 5 power 3
+# Creating a Calculator
+n1 = 19
+n2 = 9
+print("Addition of ", n1, " and ", n2, " is", n1 + n2)
+print("Subtraction of ", n1, " and ", n2, " is", n1 - n2)
+print("Multiplication of ", n1, " and ", n2, " is", n1 * n2)
+print("Division of ", n1, " and ", n2, " is", n1 / n2)
+print("Modulus of ", n1, " and ", n2, " is", n1 % n2)
+print("Floor Division of ", n1, " and ", n2, " is", n1 // n2)
