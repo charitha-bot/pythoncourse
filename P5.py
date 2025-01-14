5a58b912fb9eca9821ecb84daab4442736ec2b9f
Charitha Challa <charitha.challa522@gmail.com>
2025-01-14 22:34:58 +0530

    Add files via upload

---
+++ P5.py
@@ -0,0 +1,10 @@
+# Taking User Input
+a = input()  # platform
+print("This is a replit", a)
+b = input("Enter your name: ")
+print("My name is ", b)
+
+n1 = input("Enter the number n1: ")
+n2 = input("Enter the number n2: ")
+print(n1 + n2)  # 1213
+print(int(n1) + int(n2))  # 25
