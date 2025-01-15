a817775aa336f470f630025e507d4378ed0a1ead
Charitha Challa <charitha.challa522@gmail.com>
2025-01-15 22:05:14 +0530

    Add files via upload

---
+++ P6.py
@@ -0,0 +1,21 @@
+# Strings
+name = "python"
+apple = ' He said "He wants to eat an apple" '
+print(apple)
+form = " Replit is the \"easiest platfrom to code"
+print(form)
+# TO get multiple lines of a string then use triple single or double quote
+str = """ Hi replit
+Hi python
+Hello World"""
+print(str)
+plfrm = "Replit"
+print(plfrm[0])
+print(plfrm[1])
+print(plfrm[2])
+print(plfrm[3])
+print(plfrm[4])
+#print(plfrom[5]) # Throws an error
+print("Lets use a for loop\n")
+for character in name:
+  print(character)  # prints all the char in name line by line
