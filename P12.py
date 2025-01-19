c783e2fbc20bf086693c1d5c4eb3c8bb58c60c48
Charitha Challa <charitha.challa522@gmail.com>
2025-01-19 18:35:16 +0530

    Add files via upload

---
+++ P12.py
@@ -0,0 +1,28 @@
+# For loops in python
+name = "Replit"
+for i in name:
+  print(i)
+print("\n")
+
+# Example 1
+list = ["Red", "Green", "Blue", "Yellow"]
+for l in list:
+  print("\n")
+  print(l)
+  for char in l:
+    print(char)
+print("\n")
+
+# To print numbers
+# Use range function
+for k in range(5):
+  print(k)
+print("\n")
+
+for k in range(4):
+  print(k+1)
+print("\n")
+
+# Range(start,stop,step)
+for i in range(1 , 12, 2):
+  print(i)
