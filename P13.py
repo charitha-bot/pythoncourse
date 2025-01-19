7ea18646d919980f384e1db126b7be7f4768c567
Charitha Challa <charitha.challa522@gmail.com>
2025-01-19 19:11:06 +0530

    Add files via upload

---
+++ P13.py
@@ -0,0 +1,30 @@
+# Multiplication using for loops
+for i in range(1, 11):
+  print(2*i)
+print("\n")
+
+# # While loops
+i = 0
+while(i<=3):
+  print(i)
+  i = i+1
+print("Done with the loop")
+print("\n")
+
+# Example 1
+i = int(input("Enter the number: "))
+while(i<=38):
+  i = int(input("Enter the number: "))
+  print(i)
+print("Done with the loop")
+print("\n")# If the input is greater than 38 then it will not print the input rather it prints the last statement.
+
+# Decrementing loops
+count = 5
+while(count>0):
+  print(count)
+  count = count-1
+else:
+  print("I'm inside the else")
+  
+
