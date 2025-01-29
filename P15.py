c88159a39d41baf408cac2e84d5205d71e431efd
Charitha Challa <charitha.challa522@gmail.com>
2025-01-29 22:58:59 +0530

    Add files via upload

---
+++ P15.py
@@ -0,0 +1,8 @@
+# Functions
+# Function is a block of code that performs a specific task whenever it is called.
+def geometricmean(a, b):
+  mean = (a * b) / (a + b)
+  print(mean)
+a = 8
+b = 9
+geometricmean(a, b)
